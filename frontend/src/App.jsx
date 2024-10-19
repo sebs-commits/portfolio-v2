abcc046a701d1f0a43d60cc1e0614fa5d12ef172
Sebastian David <121909326+sebs-commits@users.noreply.github.com>
2024-10-19 12:20:48 -0400

    feat: created main page container for portfolio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,32 @@
-import React from "react";
-import './styles/App.css'
-
-
+import "./styles/App.css";
+import Navbar from "./components/Navbar";
+import AboutMe from "./pages/AboutMe";
 
 const App = () => {
   return (
-    <div className="LeftContainer">
-    <button
-      class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-      Button
-    </button>
+    <div className="min-h-screen p-4 sm:p-8 flex flex-col font-mono bg-pink-300 relative overflow-hidden">
+      {/* bunch of shapes */}
+      <div className="absolute top-0 left-0 w-32 h-32 bg-blue-400 border-2 border-black transform -rotate-12"></div>
+      <div className="absolute bottom-0 right-0 w-40 h-40 bg-green-400 border-2 border-black rounded-full"></div>
+
+      <main className="flex-grow bg-green-50 border-4 border-black shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] p-6 sm:p-10 z-10 mb-20">
+        <div className="mb-12">
+          <AboutMe />
+        </div>
+      </main>
+
+      <div className="absolute top-1/2 left-0 transform -translate-y-1/2 -translate-x-1/2 rotate-90">
+        <span
+          className="text-7xl font-black text-white stroke-black stroke-1"
+          style={{ WebkitTextStroke: "1px black" }}
+        >
+          SEBASTIAN DAVID
+        </span>
+      </div>
+
+      <Navbar />
     </div>
-  )
+  );
 };
 
 export default App;
