22bb02dbb8c385e1bd92e1eea0a3abb0ac62465b
Sebastian David <121909326+sebs-commits@users.noreply.github.com>
2024-10-19 13:56:07 -0400

    feat: changed main background to skyblue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Projects from "./pages/Projects";
 
 const App = () => {
   return (
-    <div className="min-h-screen p-4 sm:p-8 flex flex-col font-mono bg-pink-300 relative overflow-hidden">
+    <div className="min-h-screen p-4 sm:p-8 flex flex-col font-mono bg-indigo-200 relative overflow-hidden">
       {/* bunch of shapes */}
       <div className="absolute top-0 left-0 w-32 h-32 bg-blue-400 border-2 border-black transform -rotate-12"></div>
       <div className="absolute bottom-0 right-0 w-40 h-40 bg-green-400 border-2 border-black rounded-full"></div>
@@ -19,9 +19,9 @@ const App = () => {
         </div>
       </main>
 
-      <div className="absolute top-1/2 left-0 transform -translate-y-1/2 -translate-x-1/2 rotate-90">
+      <div className="absolute m-4 top-1/2 left-0 transform -translate-y-1/2 -translate-x-1/2 rotate-90">
         <span
-          className="text-7xl font-black text-white stroke-black stroke-1"
+          className="text-4xl font-black text-white stroke-black stroke-1"
           style={{ WebkitTextStroke: "1px black" }}
         >
           SEBASTIAN DAVID
