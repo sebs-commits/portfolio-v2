2c7fd1343fc391b8c6cfb67732391721536bf13e
sebdavid <121909326+sebs-commits@users.noreply.github.com>
2024-10-21 12:32:29 -0400

    chore: updated config to handle md files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import markdown from 'vite-plugin-markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    markdown({ mode: ['html', 'toc'] })
+  ],
 })
